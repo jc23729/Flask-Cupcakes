e434b2bca99ea147b1b675545c155b223cf00c8f
Juan Cruz <jc23729@gmail.com>
2021-02-09 10:42:14 -0500

    Part 1 cupcake model

--- models.py
+++ models.py
@@ -1,1 +1,15 @@
 """Models for Cupcake app."""
+"""Part One: Cupcake Model
+Create Cupcake model in models.py.
+
+It should have the following columns:
+
+id: a unique primary key that is an auto-incrementing integer
+flavor: a not-nullable text column
+size: a not-nullable text column
+rating: a not-nullable column that is a float
+image: a non-nullable text column. If an image is not given, default to https://tinyurl.com/demo-cupcake
+Make a database called cupcakes.
+
+Once you’ve made this, you can run our seed.py file to add a few sample cupcakes to your database."""
+
