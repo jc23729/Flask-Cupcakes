1e1d13cea3c5b897d97df74dee7d2a78addb992a
Juan Cruz <jc23729@gmail.com>
2021-02-09 10:53:27 -0500

    created cupcakes database
    created cupcakes database in models.py now Serializing Cupcake to a cupcake info

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+connect_db(app)
+
+
+@app.route("/")
+def root():
+    """Render homepage."""
+    return render_template('index.html')
--- models.py
+++ models.py
@@ -13,3 +13,20 @@ Make a database called cupcakes.
 
 Once you’ve made this, you can run our seed.py file to add a few sample cupcakes to your database."""
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE = "https://tinyurl.com/demo-cupcake"
+
+class Cupcake(db.Model):
+    """Cupcake"""
+    __tablename__ ="cupcakes"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
+
+    def to_dict(self):
+        """Serialize cupcake to a dict of cupcake info."""
